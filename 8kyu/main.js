ef1d14de4a37e8fc18d09ee6cbd0a02f1f0f949b
Dennise Martinez <eerika.bernal@gmail.com>
2023-08-12 22:34:08 -0400

    added 'remove first and last characters pt2 challenge'

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -650,7 +650,46 @@ The order of the sequence has to stay the same.
 
 //other solution
 
-const distinct = a = a.filter((item,index)=> a.indexOf(item)=== index)
+// const distinct = a = a.filter((item,index)=> a.indexOf(item)=== index)
 
 // indeOf finds the first appearance of an item, so if its index doesnt match the current one, the item is filtered out.
-console.log(distinct([1,1,2]))
+// console.log(distinct([1,1,2]))
+
+
+// REMOVE FIRST AND LAST CHARACTERS PT2
+
+//Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+
+//my solution
+// function array(string){
+    
+//     intArr = Array.from(String(string).split(","))
+//     if(intArr.length <= 2 ){
+//         return null
+//     }else{
+//         return intArr.slice(1,-1).join(' ').toString()
+       
+//     }
+// }
+
+
+
+//other solution
+
+// function array(arr){
+//     return arr.split(',').slice(1,-1).join(' ')|| null
+// }
+
+// console.log(array("1,2,3"))
+
+
